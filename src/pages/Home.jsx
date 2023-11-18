c54bf569ab2c69bbd62e4f0110b65bc2ce5cfa8b
sfrancop <s.francop@uniandes.edu.co>
2023-11-18 12:58:46 -0500

    Inicialización de proyecto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center flex-col bg-third h-screen'>
+        <p className='font-Comfortaa text-4xl bg-secondary w-fit p-8 rounded-lg text-white'>Binenvenido a roomatch</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#04364A',
+        'secondary': '#176B87',
+        'third': '#DAFFFB',
+      },
+      fontFamily: {
+        'Comfortaa': ['Comfortaa', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    [import('tailwindcss-animate')]
+  ],
+}
+
