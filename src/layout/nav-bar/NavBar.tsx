565ad6293e22674e4105695a84232fe0b5f2a3b3
sfrancop <s.francop@uniandes.edu.co>
2024-04-20 09:56:07 -0500

    feat: :dizzy: add tap button animation
    related to KAN-133-v2

--- src/layout/nav-bar/NavBar.tsx
+++ src/layout/nav-bar/NavBar.tsx
@@ -1,10 +1,14 @@
 import Button from "@uiComponents/button/Button"
 import "./NavBar.css"
 import RoomatchIcon from "@components/roomatch-logo/RoomatchLogo"
+import { motion, useScroll, useTransform, useViewportScroll } from "framer-motion";
 
 export default function NavBar() {
+  const { scrollYProgress } = useScroll()
+  const scale = useTransform(scrollYProgress, [0, .1], [0.8, 1]);
   return (
-    <nav className='navbar-container'>
+    <motion.nav
+      className='navbar-container'>
       <RoomatchIcon isAnimated={true}/>
       <div className='navbar__links-container'>
         <a href='#features' className='navbar__link'>Funcionalidades</a>
@@ -13,6 +17,6 @@ export default function NavBar() {
         <a href='#footer' className='navbar__link'>Contacto</a>
         <Button variant='dark' size='small' hRef="#invitation" text="Registrate"/>
       </div>
-    </nav>
+    </motion.nav>
   )
 }
