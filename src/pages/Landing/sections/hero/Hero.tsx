0cfc07e2936e2012dd03d218dce0d2cadfe83091
sfrancop <s.francop@uniandes.edu.co>
2024-06-23 14:02:43 -0500

    feat: :iphone: tablet and mobile design for navbar and hero
    related to KAN-138

--- src/pages/Landing/sections/hero/Hero.tsx
+++ src/pages/Landing/sections/hero/Hero.tsx
@@ -9,12 +9,15 @@ export default function Hero() {
   return (
     <div className='hero-container' id="hero-container">
       <motion.div
+        className="hero__text-container"
         initial={{ opacity: 0, transform: 'translateY(-50px)' }}
         whileInView={{ opacity: 1, transform: 'translateY(0)' }}
         viewport={{ once: true }}
         transition={{ duration: 0.8, ease: 'circIn'}}
       >
-        <Text isTitle={true} align='center' textColor="white" highlighted={false} text='En Roomatch encuentra tu roomie ideal, conecta y convive'/>
+        
+          <Text isTitle={true} align='center' textColor="white" highlighted={false} text='En Roomatch, encuentra tu roomie ideal, conecta y convive'/>
+        
       </motion.div>
       <div 
       className="hero__buttons-container"
