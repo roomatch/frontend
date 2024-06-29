db5c2afe545ebec9143614a0af0833a4c245f21d
sfrancop <s.francop@uniandes.edu.co>
2024-06-29 13:54:30 -0500

    fix: :lipstick: footer logo

--- src/pages/Landing/sections/footer/Footer.tsx
+++ src/pages/Landing/sections/footer/Footer.tsx
@@ -1,31 +1,35 @@
-import RoomatchIcon from '@components/roomatch-logo/RoomatchLogo'
 import './Footer.css'
 import InstagramIcon from './components/icons/InstagramIcon'
 import WhatsappIcon from './components/icons/WhatsappIcon'
 import Button from '@uiComponents/button/Button'
+import RoomatchIcon from '@assets/logo_personas.svg'
 
 export default function Footer() {
   return (
     <footer className='footer__container' id='footer' >
-        <div className="footer__logo-container">
-          <div className="footer__logo-image-container">
-            <RoomatchIcon isAnimated={false} size="small"/>
-            </div>
-            <div className="footer__icons-container">
-                <a className='footer__link' href='https://www.instagram.com/roomatch_' target='_blank'><InstagramIcon/></a>
-                <a className='footer__link' href='https://wa.me/573188601654' target='_blank'><WhatsappIcon/></a>
+      <div className="footer__logo-container">
+        <div className="footer__logo-image-container">
+        <div className='footer__logo-container'>
+                <img className='footer__logo-svg' src={RoomatchIcon} alt="" />
+                <p>roomatch</p>
+                <p className='footer__logo-slogan'>Conecta y Convive</p>
             </div>
         </div>
-        <hr className='footer__separator'/>
-        <form className='footer__form'>
-            <label className='footer__form-label'>Asunto</label>
-            <input  className='footer__input'  type="text" placeholder='Escribe el asunto de tu correo' />
-            <label className='footer__form-label'>Correo</label>
-            <input  className='footer__input' type="email" placeholder='Escribe tu direccón de correo electronico'/>
-            <label className='footer__form-label'>Mensaje</label>
-            <textarea className='footer__input' id='footer__message-input'  placeholder='Escribe tu mensaje'/>
-            <Button variant={'light'} size={'small'} type='submit' text='Enviar' className='footer__button'/>
-        </form>
+        <div className="footer__icons-container">
+          <a className='footer__link' href='https://www.instagram.com/roomatch_' target='_blank'><InstagramIcon /></a>
+          <a className='footer__link' href='https://wa.me/573188601654' target='_blank'><WhatsappIcon /></a>
+        </div>
+      </div>
+      <hr className='footer__separator' />
+      <form className='footer__form'>
+        <label className='footer__form-label'>Asunto</label>
+        <input className='footer__input' type="text" placeholder='Escribe el asunto de tu correo' />
+        <label className='footer__form-label'>Correo</label>
+        <input className='footer__input' type="email" placeholder='Escribe tu direccón de correo electronico' />
+        <label className='footer__form-label'>Mensaje</label>
+        <textarea className='footer__input' id='footer__message-input' placeholder='Escribe tu mensaje' />
+        <Button variant={'light'} size={'small'} type='submit' text='Enviar' className='footer__button' />
+      </form>
     </footer>
   )
 }
