7a712a5eae08884b4160ebc22f8ad43040c19b7c
sfrancop <s.francop@uniandes.edu.co>
2024-06-29 21:37:45 -0500

    build: :green_heart:

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,9 +1,9 @@
-import Features from './sections/features/Features'
 import Invitation from './sections/invitation/Invitation'
 import Hero from './sections/hero/Hero'
 import Why from './sections/why/Why'
 import Footer from './sections/footer/Footer'
 import Problematic from './sections/probelmatic/Problematic'
+import Features from './sections/features/Features'
 
 function Home() {
 
